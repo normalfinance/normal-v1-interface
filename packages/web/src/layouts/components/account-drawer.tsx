5813626576131bd93dc2b3c0f01c3310db41afd4
jaymalave <jaym.github@gmail.com>
2025-07-04 01:51:23 -0700

    fix: lint errors

--- packages/web/src/layouts/components/account-drawer.tsx
+++ packages/web/src/layouts/components/account-drawer.tsx
@@ -1,34 +1,34 @@
 'use client';
 
-import type { Token } from '@/types/token';
-import type { Activity } from '@/types/activity';
 import type { Connector } from '@normalfinance/types';
 import type { IconButtonProps } from '@mui/material/IconButton';
 import type { PoolDetails } from '@/components/_common/pools-explore/explorer-chart-data';
+import type { Activity } from '@/types/activity';
+import type { Token } from '@/types/token';
 
-import { useState, useEffect } from 'react';
-import { format } from '@normalfinance/utils';
+import { useEffect, useState } from 'react';
 import { useBoolean } from 'minimal-shared/hooks';
-import { hana, xbull, lobstr, freighter, useAppStore, usePersistStore } from '@normalfinance/state';
-
-import { useTranslate } from '@/locales';
-import { useTheme } from '@mui/material/styles';
+import { freighter, hana, lobstr, useAppStore, usePersistStore, xbull } from '@normalfinance/state';
+import { format } from '@normalfinance/utils';
 
 import {
   Box,
+  Button,
+  CircularProgress,
+  Drawer,
+  IconButton,
   Paper,
   Stack,
-  Drawer,
-  Button,
   Tooltip,
-  IconButton,
   Typography,
-  CircularProgress,
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
+import { useTranslate } from '@/locales';
+
+import ConnectedWallet from '@/components/_common/drawer-components/connected-wallet';
 import { Iconify } from '@/components/template/iconify';
 import { Scrollbar } from '@/components/template/scrollbar';
-import ConnectedWallet from '@/components/_common/drawer-components/connected-wallet';
 
 import { AccountButton } from './account-button';
 
