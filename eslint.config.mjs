011ab29b7146f8ae27ecdfdd50cb0d39fc5eaa00
Niko Gorjan <niko.gorjan@gmail.com>
2025-07-11 04:30:10 +0200

    chore(lint): switch scripts to eslint, keep plugin-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
+import next from 'eslint-plugin-next'
+
 export default [
   {
-    // Folders ESLint should ignore
+    plugins: { next },
+    rules: {},
     ignores: ['**/dist/**', '**/.next/**', '**/node_modules/**'],
-
-    // You can extend Next or other presets here if you like
-    // Example:
-    // plugins: { next: (await import('eslint-plugin-next')).default },
-    // rules: { ... }
-
-    // For now keep it empty; individual packages can extend further
+    languageOptions: { parserOptions: { ecmaVersion: 2022 } },
   },
-];
+]
