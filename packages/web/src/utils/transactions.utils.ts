88b18a00d322ba399053e02a604949f2c20df4a5
jaymalave <jaym.github@gmail.com>
2025-07-11 16:48:01 -0700

    fix: formatting

--- packages/web/src/utils/transactions.utils.ts
+++ packages/web/src/utils/transactions.utils.ts
@@ -69,10 +69,9 @@ export const getTransactionMessages = (details: TransactionDetails) => {
   }
 };
 
-export const getStellarExpertNetwork = () => 
+export const getStellarExpertNetwork = () =>
   // can be public or testnet, default to public, depends on the network passphrase
-   'public'
-;
+  'public';
 
 export const createStellarExpertUrl = (transactionId: string) => {
   const network = getStellarExpertNetwork();
