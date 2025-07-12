cf918eedfe64fc108f7339ca8fd441e2a0e4fff9
jaymalave <jaym.github@gmail.com>
2025-07-11 18:18:07 -0700

    (feat): remove sentry test files

--- packages/web/src/app/api/sentry-test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  throw new Error('Sentry Test API Error');
-  //the line below will never be reached, but it's here to satisfy TypeScript.
-  return NextResponse.json({ message: 'This should not be returned.' });
-}
