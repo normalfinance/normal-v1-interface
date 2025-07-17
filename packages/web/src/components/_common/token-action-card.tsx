1d8c911348b36365cf8d765612fe6d8d9057c932
jaymalave <jaym.github@gmail.com>
2025-07-17 15:17:29 -0700

    style(ui): swap card layout

--- packages/web/src/components/_common/token-action-card.tsx
+++ packages/web/src/components/_common/token-action-card.tsx
@@ -195,6 +195,7 @@ export const TokenActionCard: React.FC<TokenActionCardProps> = ({
       sx={{
         display: 'flex',
         flexDirection: 'column',
+        justifyContent: 'space-between',
         minWidth: 200,
         maxWidth: '100%',
         minHeight: 380,
