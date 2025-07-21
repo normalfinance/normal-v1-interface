d8b28bb7ad58aa42b8f2f66d374b9aeef1eb7b09
Joshua Blew <joshua@normalfinance.io>
2025-07-21 14:25:46 -0500

    fix: clean up XLM constants

--- packages/utils/src/constants/stellar.ts
+++ packages/utils/src/constants/stellar.ts
@@ -44,64 +44,8 @@ export const INSURANCE_FUND_ADDRESS: string =
 export const ORALCE_REGISTY_ADDRESS: string =
   'CCDHMFVDXDKMKIQVSWJAJJHWYCAO4M6HFFSLKSSIJ6DQVH56J2GRS7UC';
 
-export const NORMAL_BASE_FEE = '100000';
-
 /**
- * Normal History Indexer
+ * XLM token info
  */
-export const NORMAL_HISTORY_INDEXER: string = 'https://graphql.normalfinance.io';
-
-export const XLM_ADDRESS: string = 'CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA';
-
+export const XLM_ADDRESS: string = 'CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC';
 export const XLM_DECIMALS: number = 7;
-
-export const xlmTokenList = [
-  {
-    network: 'mainnet',
-    assets: [
-      {
-        contract: 'CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA',
-        name: 'StellarLumens',
-        code: 'XLM',
-        icon: 'https://assets.coingecko.com/coins/images/100/standard/Stellar_symbol_black_RGB.png',
-        decimals: 7,
-      },
-    ],
-  },
-  {
-    network: 'testnet',
-    assets: [
-      {
-        contract: 'CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC',
-        name: 'StellarLumens',
-        code: 'XLM',
-        icon: 'https://assets.coingecko.com/coins/images/100/standard/Stellar_symbol_black_RGB.png',
-        decimals: 7,
-      },
-    ],
-  },
-  {
-    network: 'standalone',
-    assets: [
-      {
-        contract: 'CDMLFMKMMD7MWZP3FKUBZPVHTUEDLSX4BYGYKH4GCESXYHS3IHQ4EIG4',
-        name: 'StellarLumens',
-        code: 'XLM',
-        icon: 'https://assets.coingecko.com/coins/images/100/standard/Stellar_symbol_black_RGB.png',
-        decimals: 7,
-      },
-    ],
-  },
-  {
-    network: 'futurenet',
-    assets: [
-      {
-        contract: 'CB64D3G7SM2RTH6JSGG34DDTFTQ5CFDKVDZJZSODMCX4NJ2HV2KN7OHT',
-        name: 'StellarLumens',
-        code: 'XLM',
-        icon: 'https://assets.coingecko.com/coins/images/100/standard/Stellar_symbol_black_RGB.png',
-        decimals: 7,
-      },
-    ],
-  },
-];
