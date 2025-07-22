eb65d8cbc56bcdb2406cd4fa2815de85f57a33e2
nikogorjan <63477501+nikogorjan@users.noreply.github.com>
2025-07-22 04:17:32 +0200

    Feature/about contact pages (#141)
    * feat: make About Us page responsive
    
    * feat: make Contact page fully responsive
    
    * fix: lint
    
    * fix: format
    
    * fix: add pages to paths
    
    * fix: verbiage and add missing translation
    
    * feat: add social links to team member card
    
    * feat: add social media links (X, LinkedIn, GitHub, Telegram) to team member cards
    
    * feat: change nikos image
    
    * feat: add “Read more” button linking to full FAQ in new tab
    
    * feat: replace contact form with email & stacked social-media icons
    
    * feat(i18n): add translation hooks to all newly added components
    
    * feat(i18n): add translation hooks to faq
    
    * feat(moments): center heading and paragraph
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Joshua Blew <joshua@normalfinance.io>
    Co-authored-by: Joshua Blew <98628839+jblewnormal@users.noreply.github.com>

---
+++ packages/web/src/sections/contact/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ContactForm from '@/components/_contact/contact-form';
+
+import { FaqAccordion } from '../landing-page/faq';
+
+const ContactView: React.FC = () => (
+  <>
+    <ContactForm />
+    <FaqAccordion />
+  </>
+);
+
+export default ContactView;
