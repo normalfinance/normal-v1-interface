060172d952f24fc8b3e897fb6509e0e5f9b1cd8f
Joshua Blew <joshua@normalfinance.io>
2025-08-20 12:36:23 -0500

    fix(TokensTab): closing tags and currency math

--- packages/web/src/components/_common/drawer-components/tokens-tab.tsx
+++ packages/web/src/components/_common/drawer-components/tokens-tab.tsx
@@ -99,54 +99,6 @@ export default function TokensTab({ tokens = [] }: { tokens?: Token[] }) {
                   </Typography>
                 </Box>
               </Box>
-            </Box>
-            <Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'flex-end',
-                  justifyContent: 'center',
-                }}
-              >
-                <Typography
-                  variant="body2"
-                  sx={{ fontWeight: 500, color: theme.palette.text.primary }}
-                >
-                  {fCurrency(token.usdValue ? Number(token.balance * BigInt(token.usdValue)) : '0')}
-                </Typography>
-                <Stack direction="row" spacing={0.5} alignItems="center" mt="4px">
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      gap: '4px',
-                      alignItems: 'flex-start',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        fontWeight: 500,
-                        color: theme.palette.text.secondary,
-                        fontSize: '12px',
-                      }}
-                    >
-                      {token.balance}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        fontWeight: 500,
-                        color: theme.palette.text.secondary,
-                        fontSize: '12px',
-                      }}
-                    >
-                      {token.symbol}
-                    </Typography>
-                  </Box>
-                </Box>
-              </Box>
               <Box>
                 <Box
                   sx={{
@@ -164,57 +116,105 @@ export default function TokensTab({ tokens = [] }: { tokens?: Token[] }) {
                   </Typography>
                   <Stack direction="row" spacing={0.5} alignItems="center" mt="4px">
                     <Box
-                      component="span"
                       sx={{
-                        width: 16,
-                        height: 16,
                         display: 'flex',
-                        borderRadius: '50%',
-                        position: 'relative',
-                        alignItems: 'center',
+                        gap: '4px',
+                        alignItems: 'flex-start',
                         justifyContent: 'center',
-                        bgcolor: varAlpha(theme.vars.palette.success.mainChannel, 0.16),
-                        color: 'success.dark',
-                        ...theme.applyStyles('dark', {
-                          color: 'success.light',
-                        }),
-                        ...(token.percentageChange &&
-                          token.percentageChange < 0 && {
-                            bgcolor: varAlpha(theme.vars.palette.error.mainChannel, 0.16),
-                            color: 'error.dark',
-                            ...theme.applyStyles('dark', {
-                              color: 'error.light',
-                            }),
-                          }),
                       }}
                     >
-                      <Iconify
-                        width={10}
-                        icon={
-                          token.percentageChange && token.percentageChange < 0
-                            ? 'eva:trending-down-fill'
-                            : 'eva:trending-up-fill'
-                        }
-                        color={
-                          token.percentageChange && token.percentageChange < 0
-                            ? 'error.main'
-                            : 'success.main'
-                        }
-                      />
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          fontWeight: 500,
+                          color: theme.palette.text.secondary,
+                          fontSize: '12px',
+                        }}
+                      >
+                        {token.balance}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          fontWeight: 500,
+                          color: theme.palette.text.secondary,
+                          fontSize: '12px',
+                        }}
+                      >
+                        {token.symbol}
+                      </Typography>
                     </Box>
-                    <Typography
-                      variant="caption"
-                      sx={{
-                        color:
-                          token.percentageChange && token.percentageChange < 0
-                            ? 'error.main'
-                            : 'success.main',
-                      }}
-                    >
-                      {token.percentageChange && token.percentageChange >= 0 && '+'}
-                      {fPercent(token.percentageChange && token.percentageChange)}
-                    </Typography>
                   </Stack>
+                </Box>
+                <Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'flex-end',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      sx={{ fontWeight: 500, color: theme.palette.text.primary }}
+                    >
+                      {fCurrency(token.usdValue * token.balance)}
+                    </Typography>
+                    <Stack direction="row" spacing={0.5} alignItems="center" mt="4px">
+                      <Box
+                        component="span"
+                        sx={{
+                          width: 16,
+                          height: 16,
+                          display: 'flex',
+                          borderRadius: '50%',
+                          position: 'relative',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          bgcolor: varAlpha(theme.vars.palette.success.mainChannel, 0.16),
+                          color: 'success.dark',
+                          ...theme.applyStyles('dark', {
+                            color: 'success.light',
+                          }),
+                          ...(token.percentageChange &&
+                            token.percentageChange < 0 && {
+                              bgcolor: varAlpha(theme.vars.palette.error.mainChannel, 0.16),
+                              color: 'error.dark',
+                              ...theme.applyStyles('dark', {
+                                color: 'error.light',
+                              }),
+                            }),
+                        }}
+                      >
+                        <Iconify
+                          width={10}
+                          icon={
+                            token.percentageChange && token.percentageChange < 0
+                              ? 'eva:trending-down-fill'
+                              : 'eva:trending-up-fill'
+                          }
+                          color={
+                            token.percentageChange && token.percentageChange < 0
+                              ? 'error.main'
+                              : 'success.main'
+                          }
+                        />
+                      </Box>
+                      <Typography
+                        variant="caption"
+                        sx={{
+                          color:
+                            token.percentageChange && token.percentageChange < 0
+                              ? 'error.main'
+                              : 'success.main',
+                        }}
+                      >
+                        {token.percentageChange && token.percentageChange >= 0 && '+'}
+                        {fPercent(token.percentageChange && token.percentageChange)}
+                      </Typography>
+                    </Stack>
+                  </Box>
                 </Box>
               </Box>
             </Button>
