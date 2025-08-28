fb91d73e4f849e51aaf093b9ee292fb3e510a7e9
Joshua Blew <joshua@normalfinance.io>
2025-08-28 13:51:07 -0500

    fix(Zealy): add missing highlights

--- packages/web/src/layouts/dashboard/layout.tsx
+++ packages/web/src/layouts/dashboard/layout.tsx
@@ -29,6 +29,8 @@ import { dashboardLayoutVars, dashboardNavColorVars } from './css-vars';
 import type { MainSectionProps } from '../core/main-section';
 import type { HeaderSectionProps } from '../core/header-section';
 import type { LayoutSectionProps } from '../core/layout-section';
+import ZealyHighlight from '@/components/_common/zealy/zealy-highlight';
+import { ZEALY_QUEST_IDS } from '@/global-config';
 
 // ----------------------------------------------------------------------
 
@@ -162,8 +164,13 @@ export function DashboardLayout({
             'You are using a testnet version of the Normal Protocol. All tokens are NOT real. You WILL experience bugs. Please report all bugs and feedback to our team. Thank you!'
           )}
           <br />
+          {/* <Box sx={{ position: 'relative' }}> */}
           <Button variant="contained" color="inherit" sx={{ mt: 1 }} onClick={handleGiveFeedback}>
             {t('Give feedback / Report bug')}
+            <ZealyHighlight
+              questId={ZEALY_QUEST_IDS.giveFeedback}
+              position={{ top: -10, right: -12 }}
+            />
           </Button>
         </Alert>
 
