f256c014de2e7904fc0514a5ad4e5447b08e65c6
Joshua Blew <joshua@normalfinance.io>
2025-08-28 21:11:25 -0500

    fix: user positions and position info

--- packages/web/src/components/_common/drawer-components/positions-tab.tsx
+++ packages/web/src/components/_common/drawer-components/positions-tab.tsx
@@ -3,68 +3,44 @@
 import type { PoolPosition } from '@/hooks';
 
 import { useTranslate } from '@/locales';
-import { fPercent } from '@/utils/format-number';
-import { getCryptoIconUrl } from '@normalfinance/utils';
+import { format, getCryptoIconUrl } from '@normalfinance/utils';
 
 import Box from '@mui/material/Box';
-import { alpha, useTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import { Stack, Avatar, Button, Typography } from '@mui/material';
 
 export interface PoolsTabsProps {
   positions?: PoolPosition[];
+  xlmPrice?: BigNumber;
 }
 
-export default function PositioinsTab({ positions = [] }: { positions: PoolPosition[] }) {
+export default function PositioinsTab({ positions = [], xlmPrice = BigNumber(0) }: PoolsTabsProps) {
   const theme = useTheme();
   const { t } = useTranslate('auto');
 
   return (
     <Box sx={{ p: 2, pt: 0 }}>
       {positions.length > 0 ? (
-        positions?.map((position) => (
-          <Button
-            key={position.poolAddress}
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              padding: 2,
-              width: '100%',
-              alignItems: 'center',
-              borderRadius: '16px',
-              border: `1px solid ${theme.palette.divider}`,
-            }}
-          >
-            <Stack direction="row" width={1} alignItems="center">
-              <Box
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'flex-start',
-                }}
-              >
-                <Avatar
-                  src={getCryptoIconUrl(position.tokenA.name ?? '')}
-                  alt="Token A"
-                  sx={{ width: 40, height: 40 }}
-                />
+        positions?.map((position) => {
+          const positionFiatValue = xlmPrice.multipliedBy(
+            format.formatTokenAmount(position.balance)
+          );
 
-                <Avatar
-                  src={getCryptoIconUrl(position.tokenB.name ?? '')}
-                  alt="Token B"
-                  sx={{
-                    width: 40,
-                    height: 40,
-                    ml: '-12px',
-                    zIndex: 1,
-                  }}
-                />
-              </Box>
-              <Stack direction="column" width={1} alignItems="start">
-                <Typography component="span" color="text.primary" variant="h6" ml={1}>
-                  {position.tokenA.name}
-                  {t('/')}
-                  {position.tokenB.name}
-                </Typography>
+          return (
+            <Button
+              key={position.poolAddress}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                padding: 2,
+                mb: 2,
+                width: '100%',
+                alignItems: 'center',
+                borderRadius: '16px',
+                border: `1px solid ${theme.palette.divider}`,
+              }}
+            >
+              <Stack direction="row" width={1} alignItems="center">
                 <Box
                   sx={{
                     display: 'flex',
@@ -72,77 +48,46 @@ export default function PositioinsTab({ positions = [] }: { positions: PoolPosit
                     justifyContent: 'flex-start',
                   }}
                 >
-                  <Box
+                  <Avatar
+                    src={getCryptoIconUrl(position.tokenA.name ?? '')}
+                    alt="Token A"
+                    sx={{ width: 25, height: 25, zIndex: 1 }}
+                  />
+
+                  <Avatar
+                    src={getCryptoIconUrl(position.tokenB.name ?? '')}
+                    alt="Token B"
                     sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start',
+                      width: 25,
+                      height: 25,
+                      ml: '-12px',
                     }}
-                  >
-                    <Typography
-                      color="text.primary"
-                      variant="caption"
-                      ml={1}
-                      sx={{
-                        backgroundColor: alpha(theme.palette.grey[500], 0.08),
-                        border: `1px solid ${theme.palette.divider}`,
-                        borderRadius: '4px',
-                        px: '6px',
-                        py: '2px',
-                      }}
-                    >
-                      {fPercent(position.poolFee)}
-                    </Typography>
-                  </Box>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start',
-                    }}
-                  >
-                    <Typography
-                      color="text.primary"
-                      variant="caption"
-                      ml={1}
-                      sx={{
-                        backgroundColor: alpha(theme.palette.grey[500], 0.08),
-                        border: `1px solid ${theme.palette.divider}`,
-                        borderRadius: '4px',
-                        px: '6px',
-                        py: '2px',
-                      }}
-                    >
-                      {position.poolVersion}
-                    </Typography>
-                  </Box>
+                  />
                 </Box>
-              </Stack>
-            </Stack>
-
-            <Stack direction="row" width={1} mt={4} gap={3} alignItems="start">
-              <Stack direction="column" alignItems="start">
-                <Typography color="text.primary" variant="body1">
-                  {/* {fCurrency(pool.performance?.position)} */}
-                  {position.balance}
-                </Typography>
-                <Typography color="text.secondary" variant="caption">
-                  {t('Position')}
-                </Typography>
+                <Stack direction="column" width={1} alignItems="start">
+                  <Typography component="span" color="text.primary" variant="h6" ml={1}>
+                    {position.tokenA.name}
+                    {t('/')}
+                    {position.tokenB.name}
+                  </Typography>
+                </Stack>
               </Stack>
 
-              <Stack direction="column" alignItems="start">
-                <Typography color="text.primary" variant="body1">
-                  {/* {fCurrency(position.performance?.fees)} */}
-                  {t('Coming soon')}
-                </Typography>
-                <Typography color="text.secondary" variant="caption">
-                  {t('Fees')}
-                </Typography>
+              <Stack direction="row" width={1} mt={4} gap={3} alignItems="start">
+                <Stack direction="column" alignItems="start">
+                  <Typography color="text.primary" variant="body1">
+                    {/* eslint-disable-next-line i18next/no-literal-string */}
+                    {format.formatTokenAmount(position.balance)} XLM (
+                    {format.fCurrency(positionFiatValue.toFixed(2))})
+                  </Typography>
+                  <Typography color="text.secondary" variant="caption">
+                    {t('Liquidity Provided')}
+                  </Typography>
+                </Stack>
               </Stack>
-            </Stack>
-          </Button>
-        ))
+            </Button>
+          );
+        })
       ) : (
         <Typography>{t('No tokens match your search.')}</Typography>
       )}
--- packages/web/src/hooks/stellar/use-liquidity-positions.ts
+++ packages/web/src/hooks/stellar/use-liquidity-positions.ts
@@ -5,8 +5,8 @@ import type { StateToken as Token } from '@normalfinance/types';
 import { captureException } from '@sentry/nextjs';
 import { usePersistStore } from '@normalfinance/state';
 import { useState, useEffect, useCallback } from 'react';
-import { format, constants, getCryptoIconUrl } from '@normalfinance/utils';
-import { PoolRouterContract, SorobanTokenContract } from '@normalfinance/contracts';
+import { PoolRouterContract } from '@normalfinance/contracts';
+import { format, constants, getTokenBalance, getCryptoIconUrl } from '@normalfinance/utils';
 
 // ----------------------------------------------------------------------
 
@@ -44,35 +44,9 @@ export function useLiquidityPositions(): ReturnType {
     let position: PoolPosition | undefined;
 
     const tokenAddress = poolInfo.pool_response.token_share.address;
+    const balance = await getTokenBalance(tokenAddress, usePersistStore.getState().wallet.address!);
 
-    const TokenContract = new SorobanTokenContract.Client({
-      contractId: tokenAddress.toString(),
-      networkPassphrase: constants.StellarConfig.NETWORK_PASSPHRASE,
-      rpcUrl: constants.StellarConfig.RPC_URL,
-    });
-
-    // BALANCE
-    let balance: bigint;
-    try {
-      balance = (
-        await TokenContract.balance({
-          id: usePersistStore.getState().wallet.address!,
-        })
-      ).result;
-    } catch (e) {
-      balance = BigInt(0);
-    }
-
-    // SYMBOL
-    let symbol: string;
-    try {
-      symbol = (await TokenContract.symbol()).result;
-    } catch (e) {
-      return position;
-    }
-
-    // DECIMALS
-    const decimals = Number((await TokenContract.decimals()).result);
+    if (Number(balance) == 0) return position;
 
     const normalTokenSymbol = format.formatNormalToken(
       poolInfo.pool_response.pool.base_asset,
@@ -84,7 +58,7 @@ export function useLiquidityPositions(): ReturnType {
       tokenAddress: poolInfo.pool_response.token_share.address,
       tokenA: {
         id: poolInfo.pool_response.token_a.address,
-        decimals,
+        decimals: 7,
         symbol: normalTokenSymbol,
         name: normalTokenSymbol,
         icon: getCryptoIconUrl(
@@ -97,7 +71,7 @@ export function useLiquidityPositions(): ReturnType {
       },
       tokenB: {
         id: poolInfo.pool_response.token_b.address,
-        decimals,
+        decimals: 7,
         symbol: 'XLM',
         name: poolInfo.pool_response.pool.quote_asset,
         icon: getCryptoIconUrl(poolInfo.pool_response.pool.quote_asset),
@@ -140,7 +114,9 @@ export function useLiquidityPositions(): ReturnType {
         : [];
 
       const data = await Promise.all(allPositions);
-      setPostions(data as PoolPosition[]);
+
+      const userPositions = data.filter((e) => e !== undefined);
+      setPostions(userPositions as PoolPosition[]);
     } catch (e: any) {
       captureException(e);
       setError(e);
