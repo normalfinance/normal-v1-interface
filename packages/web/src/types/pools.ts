7763df936099d4aff56363ce92d000fd2ce81673
Joshua Blew <joshua@normalfinance.io>
2025-08-28 21:13:15 -0500

    feat: add delta_a to Router events

--- packages/web/src/types/pools.ts
+++ packages/web/src/types/pools.ts
@@ -2,9 +2,9 @@ export type TxType = 'Buy' | 'Sell' | 'Deposit' | 'Withdraw';
 
 export interface PoolTxRow {
   type: TxType;
-  // usdValue: number; // coming soon
   tokenAAmount: BigNumber;
   tokenBAmount: BigNumber;
+  deltaA: BigNumber;
   user: string;
   timestamp: number;
   txHash: string;
