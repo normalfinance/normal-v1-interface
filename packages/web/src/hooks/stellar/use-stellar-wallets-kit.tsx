217c49e43075c1dc9f056bff9c912e34f3a424df
jaymalave <jaym.github@gmail.com>
2025-09-12 13:37:04 -0600

    Feat: format and lint

--- packages/web/src/hooks/stellar/use-stellar-wallets-kit.tsx
+++ packages/web/src/hooks/stellar/use-stellar-wallets-kit.tsx
@@ -1,10 +1,8 @@
 import { useRef, useEffect, useCallback } from 'react';
 import { usePersistStore, useStellarWalletKitStore } from '@normalfinance/state';
-import { HANA_ID, XBULL_ID, LOBSTR_ID, FREIGHTER_ID } from '@creit.tech/stellar-wallets-kit';
-
-import { WALLET_CONNECT_ID } from '@creit.tech/stellar-wallets-kit/modules/walletconnect.module';
-
 import { LEDGER_ID } from '@creit.tech/stellar-wallets-kit/modules/ledger.module';
+import { HANA_ID, XBULL_ID, LOBSTR_ID, FREIGHTER_ID } from '@creit.tech/stellar-wallets-kit';
+import { WALLET_CONNECT_ID } from '@creit.tech/stellar-wallets-kit/modules/walletconnect.module';
 
 export const useStellarWalletsKit = () => {
   const persistStore = usePersistStore();
