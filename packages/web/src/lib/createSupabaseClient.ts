0674ad8ba2776f470e9f57ca6c78b0acbf601d1e
jaymalave <jaym.github@gmail.com>
2025-09-17 17:38:46 -0600

    Feat: supabase client to use correct env

--- packages/web/src/lib/createSupabaseClient.ts
+++ packages/web/src/lib/createSupabaseClient.ts
@@ -3,13 +3,13 @@ import { constants } from '@normalfinance/utils';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = constants.getNetworkConfig(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_MAINNET_URL || process.env.NEXT_PUBLIC_SUPABASE_URL!
+  process.env.NEXT_PUBLIC_TESTNET_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_MAINNET_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL! || ''
 );
 
 const supabaseAnonKey = constants.getNetworkConfig(
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  process.env.NEXT_PUBLIC_SUPABASE_MAINNET_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_TESTNET_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_MAINNET_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
