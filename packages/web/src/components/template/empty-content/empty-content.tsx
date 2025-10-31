bd3134a04f69eed471adf24d72e5f25ad2172798
Joshua Blew <joshua@normalfinance.io>
2025-10-31 13:32:05 -0500

    fix: lint

--- packages/web/src/components/template/empty-content/empty-content.tsx
+++ packages/web/src/components/template/empty-content/empty-content.tsx
@@ -2,12 +2,12 @@ import type { BoxProps } from '@mui/material/Box';
 import type { Theme, SxProps } from '@mui/material/styles';
 import type { TypographyProps } from '@mui/material/Typography';
 
+import { cdn } from '@normalfinance/utils';
 import { varAlpha } from 'minimal-shared/utils';
 
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import { cdn } from '@normalfinance/utils';
 
 // ----------------------------------------------------------------------
 
