1bd8d59ab1ff471d5fcf976f2d8c89e0d5f4a56a
jaymalave <jaym.github@gmail.com>
2025-12-05 17:49:05 +0530

    fix: supabase client for auth

--- packages/web/src/lib/createSupabaseClient.ts
+++ packages/web/src/lib/createSupabaseClient.ts
@@ -12,7 +12,14 @@ const supabaseAnonKey = constants.getNetworkConfig(
   process.env.NEXT_PUBLIC_MAINNET_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    flowType: 'pkce',
+  },
+});
 
 if (typeof window !== 'undefined') {
   logger.log(`[Supabase] Connected to ${constants.getCurrentNetwork()} database`);
