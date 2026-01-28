f25afdfdcec3dd6578bebba877db3705476efa43
Joshua Blew <joshua@normalfinance.io>
2026-01-28 08:34:55 -0600

    fix(settings): temp remove metadata

--- packages/web/src/app/settings/page.tsx
+++ packages/web/src/app/settings/page.tsx
@@ -1,51 +1,52 @@
 'use client';
 
-import type { Metadata } from 'next';
-
 import { useEffect } from 'react';
-import { CONFIG } from '@/global-config';
 import { useRouter } from 'next/navigation';
 import { UserSettingsView } from '@/sections/settings';
 import { useSupabaseAuth } from '@/providers/SupabaseAuthProvider';
 
-export const metadata: Metadata = {
-  title: {
-    default: 'Settings | Normal',
-    template: '%s · Normal',
-  },
-  description: 'View and manage your Normal account settings.',
-  alternates: {
-    canonical: '/assets',
-  },
-  openGraph: {
-    title: 'Settings | Normal',
-    description: 'View and manage your Normal account settings.',
-    url: `${CONFIG.siteUrl}/legal/disclaimer`,
-    siteName: 'Normal',
-    images: [
-      {
-        url: '/og/home.png', // replace with image you want to show when sharing link on other socials than twitter
-        width: 1200,
-        height: 630,
-        alt: 'Normal Settings',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Settings | Normal',
-    description: 'View and manage your Normal account settings.',
-    images: ['/og/home.png'], // replace with image you want to show when sharing link on Twitter
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: { index: true, follow: true },
-  },
-  keywords: ['Normal', 'Normal Finance', 'settings'],
-};
+// @dev Creates error:
+// You are attempting to export "metadata" from a component marked with "use client", which is disallowed.
+// Either remove the export, or the "use client" directive. Read more: https://nextjs.org/
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: 'Settings | Normal',
+//     template: '%s · Normal',
+//   },
+//   description: 'View and manage your Normal account settings.',
+//   alternates: {
+//     canonical: '/assets',
+//   },
+//   openGraph: {
+//     title: 'Settings | Normal',
+//     description: 'View and manage your Normal account settings.',
+//     url: `${CONFIG.siteUrl}/legal/disclaimer`,
+//     siteName: 'Normal',
+//     images: [
+//       {
+//         url: '/og/home.png', // replace with image you want to show when sharing link on other socials than twitter
+//         width: 1200,
+//         height: 630,
+//         alt: 'Normal Settings',
+//       },
+//     ],
+//     locale: 'en_US',
+//     type: 'website',
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: 'Settings | Normal',
+//     description: 'View and manage your Normal account settings.',
+//     images: ['/og/home.png'], // replace with image you want to show when sharing link on Twitter
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: { index: true, follow: true },
+//   },
+//   keywords: ['Normal', 'Normal Finance', 'settings'],
+// };
 
 export default function SettingsPage() {
   const { session, isLoading } = useSupabaseAuth();
