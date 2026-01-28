11ae9e4931cba28a781d129ffcc5af5d3d804f18
Joshua Blew <98628839+jblewnormal@users.noreply.github.com>
2026-01-28 08:11:11 -0600

    Fix: update page metadata (#370)
    * fix(web): update page metadata
    
    * chore(web): formatting

--- packages/web/src/app/legal/tos/page.tsx
+++ packages/web/src/app/legal/tos/page.tsx
@@ -1,10 +1,46 @@
 import type { Metadata } from 'next';
 
 import TermsOfServiceView from '@/sections/legal/tos-view';
+import { CONFIG } from '@/global-config';
+import { paths } from '@/routes/paths';
 
 export const metadata: Metadata = {
-  title: 'Normal | Legal - Terms of Service',
-  description: '',
+  title: {
+    default: 'Legal - Terms of Service | Normal',
+    template: '%s · Normal',
+  },
+  description: 'View our official Terms of Service.',
+  alternates: {
+    canonical: '/assets',
+  },
+  openGraph: {
+    title: 'Legal - Terms of Service | Normal',
+    description: 'View our official Terms of Service.',
+    url: `${CONFIG.siteUrl}${paths.legal.tos}`,
+    siteName: 'Normal',
+    images: [
+      {
+        url: '/og/home.png', // replace with image you want to show when sharing link on other socials than twitter
+        width: 1200,
+        height: 630,
+        alt: 'Normal Legal - Terms of Service',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Legal - Terms of Service | Normal',
+    description: 'View our official Terms of Service.',
+    images: ['/og/home.png'], // replace with image you want to show when sharing link on Twitter
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: { index: true, follow: true },
+  },
+  keywords: ['Normal', 'Normal Finance', 'legal', 'terms of service'],
 };
 
 export default function Page() {
