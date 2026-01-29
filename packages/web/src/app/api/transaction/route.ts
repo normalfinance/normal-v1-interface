5d40428d152d9cbd0e1b8f4cdd6a02c1559116a4
Joshua Blew <joshua@normalfinance.io>
2026-01-29 02:05:40 -0600

    fix(api/transaction): add better transaction status handling

--- packages/web/src/app/api/transaction/route.ts
+++ packages/web/src/app/api/transaction/route.ts
@@ -119,21 +119,88 @@ async function transactionHandler(req: NextRequest) {
       logger.log('[Transaction API] Stellar Server: ', rpcServer);
 
       // Submit the signed transaction
-      const result = await rpcServer.sendTransaction(
-        new Transaction(signedTransactionXDR, constants.StellarConfig.NETWORK_PASSPHRASE)
+      const transaction = new Transaction(
+        signedTransactionXDR,
+        constants.StellarConfig.NETWORK_PASSPHRASE
       );
+      let sendTxResponse = await rpcServer.sendTransaction(transaction);
 
-      logger.log('[Transaction API] Result server.sendTransaction: ', result);
+      logger.log('[Transaction API] Result server.sendTransaction: ', sendTxResponse);
 
-      return NextResponse.json({
-        success: true,
-        transactionHash: result.hash,
-        result,
-        config: {
-          timeout: apiConfig.timeout,
-          rateLimitRemaining: remaining,
-        },
-      });
+      let currentTime = Date.now();
+
+      // Attempt to send the transaction and poll for the result
+      while (sendTxResponse.status !== 'PENDING' && Date.now() - currentTime < 5000) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        sendTxResponse = await rpcServer.sendTransaction(transaction);
+      }
+      if (sendTxResponse.status !== 'PENDING') {
+        const error = parseError(sendTxResponse);
+        logger.log('[Transaction API] Failed to send transaction: ', sendTxResponse.hash, error);
+
+        return NextResponse.json(
+          {
+            error:
+              ContractErrorType[error.type] ||
+              `Contract execution failed on send: ${JSON.stringify(sendTxResponse.errorResult)}`,
+          },
+          { status: 500 }
+        );
+      }
+
+      currentTime = Date.now();
+
+      let getTxResponse = await rpcServer.getTransaction(sendTxResponse.hash);
+
+      while (getTxResponse.status === 'NOT_FOUND' && Date.now() - currentTime < 30000) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        getTxResponse = await rpcServer.getTransaction(sendTxResponse.hash);
+      }
+
+      if (getTxResponse.status === 'NOT_FOUND') {
+        logger.log(
+          '[Transaction API] Unable to validate transaction success: ',
+          getTxResponse.txHash
+        );
+
+        return NextResponse.json(
+          {
+            error:
+              'The transaction could have been accepted by the network, but we were unable to verify.',
+          },
+          { status: 500 }
+        );
+      }
+
+      const hash = transaction.hash().toString('hex');
+
+      if (getTxResponse.status === 'SUCCESS') {
+        logger.log('[Transaction API] Successfully submitted transaction: ', hash);
+        // stall for a bit to ensure data propagates to horizon
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        return NextResponse.json({
+          success: true,
+          transactionHash: getTxResponse.txHash,
+          getTxResponse,
+          config: {
+            timeout: apiConfig.timeout,
+            rateLimitRemaining: remaining,
+          },
+        });
+      } else {
+        const error = parseError(getTxResponse);
+        logger.error(`[Transaction API] Transaction failed: `, hash, error);
+
+        return NextResponse.json(
+          {
+            error:
+              ContractErrorType[error.type] ||
+              `Contract execution failed on get: ${JSON.stringify(getTxResponse.resultXdr)}`,
+          },
+          { status: 500 }
+        );
+      }
     } catch (contractError: any) {
       logger.log('[Transaction API] Contract error: ', contractError);
       await logWithConfig('error', `${transactionType || 'Contract'} execution failed`, {
@@ -162,3 +229,215 @@ async function transactionHandler(req: NextRequest) {
 
 // export const POST = createEdgeConfigHandler(transactionHandler, 'transaction');
 export const POST = createNodeConfigHandler(transactionHandler, 'transaction');
+
+export class ContractError extends Error {
+  /**
+   * The type of the error
+   */
+  public type: ContractErrorType;
+
+  constructor(type: ContractErrorType) {
+    super();
+    this.type = type;
+  }
+}
+
+export enum ContractErrorType {
+  UnknownError = -1000,
+
+  // Transaction Submission Errors
+  txSorobanInvalid = -24,
+  txMalformed = -23,
+  txBadMinSeqAgeOrGap = -22,
+  txBadSponsorship = -21,
+  txFeeBumpInnerFailed = -20,
+  txNotSupported = -19,
+  txInternalError = -18,
+  txBadAuthExtra = -17,
+  txInsufficientFee = -16,
+  txNoAccount = -15,
+  txInsufficientBalance = -14,
+  txBadAuth = -13,
+  txBadSeq = -12,
+  txMissingOperation = -11,
+  txTooLate = -10,
+  txTooEarly = -9,
+
+  // Host Function Errors
+  InvokeHostFunctionInsufficientRefundableFee = -5,
+  InvokeHostFunctionEntryArchived = -4,
+  InvokeHostFunctionResourceLimitExceeded = -3,
+  InvokeHostFunctionTrapped = -2,
+  InvokeHostFunctionMalformed = -1,
+
+  // Common Errors
+  InternalError = 1,
+  OperationNotSupportedError = 2,
+  AlreadyInitializedError = 3,
+
+  UnauthorizedError = 4,
+  AuthenticationError = 5,
+  AccountMissingError = 6,
+  AccountIsNotClassic = 7,
+
+  NegativeAmountError = 8,
+  AllowanceError = 9,
+  BalanceError = 10,
+  BalanceDeauthorizedError = 11,
+  OverflowError = 12,
+  TrustlineMissingError = 13,
+
+  // Potential Comet Errors
+  CometErrFreezeOnlyWithdrawals = 14,
+  CometErrMaxInRatio = 17,
+  CometErrMathApprox = 18,
+  CometErrLimitIn = 19,
+  CometErrLimitOut = 20,
+  CometErrMaxOutRatio = 21,
+  CometErrBadLimitPrice = 22,
+  CometErrLimitPrice = 23,
+  CometErrTokenAmountIsNegative = 25,
+  CometErrInsufficientAllowance = 27,
+  CometErrInsufficientBalance = 29,
+  CometErrAddOverflow = 30,
+  CometErrSubUnderflow = 31,
+  CometErrDivInternal = 32,
+  CometErrMulOverflow = 33,
+  CometErrCPowBaseTooLow = 34,
+  CometErrCPowBaseTooHigh = 35,
+  CometErrInvalidExpirationLedger = 36,
+  CometErrNegativeOrZero = 37,
+  CometErrTokenInvalid = 38,
+
+  // Backstop
+  BackstopBadRequest = 1000,
+  NotExpired = 1001,
+  InvalidRewardZoneEntry = 1002,
+  InsufficientFunds = 1003,
+  NotPool = 1004,
+  InvalidShareMintAmount = 1005,
+  InvalidTokenWithdrawAmount = 1006,
+  TooManyQ4WEntries = 1007,
+  NotInRewardZone = 1008,
+  RewardZoneFull = 1009,
+  MaxBackfillEmissions = 1010,
+  BadDebtExists = 1011,
+
+  // Pool Request Errors (start at 1200)
+  PoolBadRequest = 1200,
+  InvalidPoolInitArgs = 1201,
+  InvalidReserveMetadata = 1202,
+  InitNotUnlocked = 1203,
+  StatusNotAllowed = 1204,
+
+  // Pool State Errors
+  InvalidHf = 1205,
+  InvalidPoolStatus = 1206,
+  InvalidUtilRate = 1207,
+  MaxPositionsExceeded = 1208,
+  InternalReserveNotFound = 1209,
+  InvalidBTokenMintAmount = 1216,
+  InvalidBTokenBurnAmount = 1217,
+  InvalidDTokenMintAmount = 1218,
+  InvalidDTokenBurnAmount = 1219,
+  ExceededSupplyCap = 1220,
+  ReserveDisabled = 1223,
+  MinCollateralNotMet = 1224,
+
+  // Oracle Errors
+  StalePrice = 1210,
+
+  // Auction Errors
+  InvalidLiquidation = 1211,
+  AuctionInProgress = 1212,
+  InvalidLiqTooLarge = 1213,
+  InvalidLiqTooSmall = 1214,
+  InterestTooSmall = 1215,
+  InvalidBid = 1221,
+  InvalidLot = 1222,
+
+  // Pool Factory
+  InvalidPoolFactoryInitArgs = 1300,
+}
+
+export function parseError(
+  errorResponse:
+    | rpc.Api.GetFailedTransactionResponse
+    | rpc.Api.SendTransactionResponse
+    | rpc.Api.SimulateTransactionErrorResponse
+): ContractError {
+  // Simulation Error
+  if ('id' in errorResponse) {
+    const match = errorResponse.error.match(/Error\(Contract, #(\d+)\)/);
+    if (match) {
+      const errorValue = parseInt(match[1], 10);
+      if (errorValue in ContractErrorType)
+        return new ContractError(errorValue as ContractErrorType);
+    }
+    return new ContractError(ContractErrorType.UnknownError);
+  }
+
+  // Send Transaction Error
+  if (
+    errorResponse &&
+    typeof errorResponse === 'object' &&
+    'errorResult' in errorResponse &&
+    errorResponse.errorResult
+  ) {
+    const txErrorName = errorResponse.errorResult.result().switch().name;
+    if (txErrorName == 'txFailed') {
+      // Transaction should only contain one operation
+      if (errorResponse.errorResult.result().results().length == 1) {
+        const hostFunctionError = errorResponse.errorResult
+          .result()
+          .results()[0]
+          .tr()
+          .invokeHostFunctionResult()
+          .switch().value;
+        if (hostFunctionError in ContractErrorType)
+          return new ContractError(hostFunctionError as ContractErrorType);
+      }
+    } else {
+      const txErrorValue = errorResponse.errorResult.result().switch().value - 7;
+      if (txErrorValue in ContractErrorType) {
+        return new ContractError(txErrorValue as ContractErrorType);
+      }
+    }
+  }
+
+  // Get Transaction Error
+  if (
+    errorResponse &&
+    typeof errorResponse === 'object' &&
+    'resultXdr' in errorResponse &&
+    errorResponse.resultXdr
+  ) {
+    // Transaction submission failed
+    const txResult = errorResponse.resultXdr.result();
+    const txErrorName = txResult.switch().name;
+
+    // Use invokeHostFunctionErrors in case of generic `txFailed` error
+    if (txErrorName == 'txFailed') {
+      // Transaction should only contain one operation
+      if (errorResponse.resultXdr.result().results().length == 1) {
+        const hostFunctionError = txResult
+          .results()[0]
+          .tr()
+          .invokeHostFunctionResult()
+          .switch().value;
+        if (hostFunctionError in ContractErrorType)
+          return new ContractError(hostFunctionError as ContractErrorType);
+      }
+    }
+
+    // Shift the error value to avoid collision with invokeHostFunctionErrors
+    const txErrorValue = txResult.switch().value - 7;
+    // Use TransactionResultCode with more specific errors
+    if (txErrorValue in ContractErrorType) {
+      return new ContractError(txErrorValue as ContractErrorType);
+    }
+  }
+
+  // If the error is not recognized, return an unknown error
+  return new ContractError(ContractErrorType.UnknownError);
+}
