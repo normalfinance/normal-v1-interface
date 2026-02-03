535ed5bd8f508c94637779865d266aace2a69a15
Joshua Blew <joshua@normalfinance.io>
2026-02-03 10:45:46 -0600

    fix(http): import issue

--- packages/web/src/utils/http.ts
+++ packages/web/src/utils/http.ts
@@ -1,5 +1,6 @@
-import type { NextRequest, NextResponse, type NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server';
 
+import { NextResponse } from 'next/server';
 import { supabase } from '@/lib/createSupabaseClient';
 
 export function getClientIP(request: NextRequest): string {
