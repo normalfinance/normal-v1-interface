0523495abb0389c0ca44675b3fcd2e50e85d610e
nikogorjan <niko.gorjan@gmail.com>
2026-04-24 17:14:46 +0200

    help and feedback go to discord

--- packages/web/src/routes/paths.ts
+++ packages/web/src/routes/paths.ts
@@ -46,7 +46,7 @@ export const paths = {
   },
   help: {
     buy: 'https://docs.normalfinance.io', // FIXME: update to help center when ready
-    feedbackForm: 'https://link.normalfinance.io/feedback',
+    feedbackForm: 'https://discord.gg/KUG3qhQz',
     contractTracker: 'https://normalfinance.notion.site/mainnet-v1',
   },
 };
