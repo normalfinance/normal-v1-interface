775837378d9777cf473ea3a3b2eb7e28315ab3c0
nikogorjan <niko.gorjan@gmail.com>
2026-05-10 23:46:19 +0200

    link to new docs

--- packages/web/src/routes/paths.ts
+++ packages/web/src/routes/paths.ts
@@ -21,7 +21,7 @@ export const paths = {
     instagram: 'https://www.instagram.com/normalfinance.io',
   },
   blog: 'https://normalfi.substack.com/',
-  docs: 'https://normalfinance.gitbook.io/docs',
+  docs: 'https://normalfi.gitbook.io/normal',
   // main
   swap: '/swap',
   /** Same destination as the /invest → /swap permanent redirect */
